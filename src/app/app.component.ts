cfa82aabda8fccd986698497133b0c4d00fe8959
CristobalAraneda <cristobal.a85@gmail.com>
2018-10-17 14:03:05 -0300

    fin de la seccion 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { InfoPaginaService } from './service/info-pagina.service';
+import { ProductosService } from './service/productos.service';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,8 @@ import { InfoPaginaService } from './service/info-pagina.service';
 export class AppComponent {
   // title = 'portafolio';
 
-  constructor( public infoPaginaService: InfoPaginaService ) {
+  constructor( public infoPaginaService: InfoPaginaService ,
+               public productosService: ProductosService) {
   }
 
 }
